e372d6637f4dba61f9ce26f4c65f4fb5674b0826
omidafifi <afifiomid@yahoo.com>
2025-09-02 23:22:40 +0330

    minimal web server in Express.js

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+
+app.get("/", (req, res) => res.send("Hello Express")); //home
+
+app.get("/users", (req, res) => {
+  const users = [
+    {
+      name1: "omid",
+      age: 34,
+      name2: "ahmad",
+      age: 25,
+      name3: "farhad",
+      age: 44,
+    },
+  ];
+  res.send(users);
+});
+
 app.listen(PORT, () => {
   console.log(`click to join localhost http://localhost:${PORT}`);
 });
